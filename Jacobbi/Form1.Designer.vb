d291e5eefebdebc0fa1d384474b1a94bba5b76a7
zeris <brandon-gg1@outlook.com>
2018-09-23 22:34:19 -0500

    Innitial commit

---
+++ Jacobbi/Form1.Designer.vb
@@ -0,0 +1,445 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle7 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.b3 = New System.Windows.Forms.TextBox()
+        Me.b2 = New System.Windows.Forms.TextBox()
+        Me.b1 = New System.Windows.Forms.TextBox()
+        Me.a33 = New System.Windows.Forms.TextBox()
+        Me.a23 = New System.Windows.Forms.TextBox()
+        Me.a13 = New System.Windows.Forms.TextBox()
+        Me.a32 = New System.Windows.Forms.TextBox()
+        Me.a22 = New System.Windows.Forms.TextBox()
+        Me.a12 = New System.Windows.Forms.TextBox()
+        Me.a31 = New System.Windows.Forms.TextBox()
+        Me.a21 = New System.Windows.Forms.TextBox()
+        Me.a11 = New System.Windows.Forms.TextBox()
+        Me.tcs = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.salida = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Respuesta = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.salida, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.b3)
+        Me.Panel1.Controls.Add(Me.b2)
+        Me.Panel1.Controls.Add(Me.b1)
+        Me.Panel1.Controls.Add(Me.a33)
+        Me.Panel1.Controls.Add(Me.a23)
+        Me.Panel1.Controls.Add(Me.a13)
+        Me.Panel1.Controls.Add(Me.a32)
+        Me.Panel1.Controls.Add(Me.a22)
+        Me.Panel1.Controls.Add(Me.a12)
+        Me.Panel1.Controls.Add(Me.a31)
+        Me.Panel1.Controls.Add(Me.a21)
+        Me.Panel1.Controls.Add(Me.a11)
+        Me.Panel1.Location = New System.Drawing.Point(12, 101)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(435, 231)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(316, 179)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(18, 13)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Text = "z="
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(316, 105)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(18, 13)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "z="
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(316, 23)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(18, 13)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "z="
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(192, 179)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(12, 13)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "y"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(192, 105)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(12, 13)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "y"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(192, 23)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(12, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "y"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(79, 179)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(12, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "x"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(79, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "x"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(79, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(12, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "x"
+        '
+        'b3
+        '
+        Me.b3.Location = New System.Drawing.Point(364, 176)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(36, 20)
+        Me.b3.TabIndex = 11
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(364, 98)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(36, 20)
+        Me.b2.TabIndex = 10
+        '
+        'b1
+        '
+        Me.b1.Location = New System.Drawing.Point(364, 20)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(36, 20)
+        Me.b1.TabIndex = 9
+        '
+        'a33
+        '
+        Me.a33.Location = New System.Drawing.Point(237, 176)
+        Me.a33.Name = "a33"
+        Me.a33.Size = New System.Drawing.Size(36, 20)
+        Me.a33.TabIndex = 8
+        '
+        'a23
+        '
+        Me.a23.Location = New System.Drawing.Point(237, 98)
+        Me.a23.Name = "a23"
+        Me.a23.Size = New System.Drawing.Size(36, 20)
+        Me.a23.TabIndex = 7
+        '
+        'a13
+        '
+        Me.a13.Location = New System.Drawing.Point(237, 20)
+        Me.a13.Name = "a13"
+        Me.a13.Size = New System.Drawing.Size(36, 20)
+        Me.a13.TabIndex = 6
+        '
+        'a32
+        '
+        Me.a32.Location = New System.Drawing.Point(120, 176)
+        Me.a32.Name = "a32"
+        Me.a32.Size = New System.Drawing.Size(36, 20)
+        Me.a32.TabIndex = 5
+        '
+        'a22
+        '
+        Me.a22.Location = New System.Drawing.Point(120, 98)
+        Me.a22.Name = "a22"
+        Me.a22.Size = New System.Drawing.Size(36, 20)
+        Me.a22.TabIndex = 4
+        '
+        'a12
+        '
+        Me.a12.Location = New System.Drawing.Point(120, 20)
+        Me.a12.Name = "a12"
+        Me.a12.Size = New System.Drawing.Size(36, 20)
+        Me.a12.TabIndex = 3
+        '
+        'a31
+        '
+        Me.a31.Location = New System.Drawing.Point(19, 176)
+        Me.a31.Name = "a31"
+        Me.a31.Size = New System.Drawing.Size(36, 20)
+        Me.a31.TabIndex = 2
+        '
+        'a21
+        '
+        Me.a21.Location = New System.Drawing.Point(19, 98)
+        Me.a21.Name = "a21"
+        Me.a21.Size = New System.Drawing.Size(36, 20)
+        Me.a21.TabIndex = 1
+        '
+        'a11
+        '
+        Me.a11.Location = New System.Drawing.Point(19, 20)
+        Me.a11.Name = "a11"
+        Me.a11.Size = New System.Drawing.Size(36, 20)
+        Me.a11.TabIndex = 0
+        '
+        'tcs
+        '
+        Me.tcs.Location = New System.Drawing.Point(246, 43)
+        Me.tcs.Name = "tcs"
+        Me.tcs.Size = New System.Drawing.Size(100, 20)
+        Me.tcs.TabIndex = 1
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(31, 46)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(190, 13)
+        Me.Label10.TabIndex = 2
+        Me.Label10.Text = "Dame el número de cifras significativas"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(525, 136)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 3
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(525, 277)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'salida
+        '
+        Me.salida.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.salida.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7})
+        Me.salida.Location = New System.Drawing.Point(12, 374)
+        Me.salida.Name = "salida"
+        Me.salida.Size = New System.Drawing.Size(620, 150)
+        Me.salida.TabIndex = 5
+        '
+        'Column1
+        '
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter
+        Me.Column1.DefaultCellStyle = DataGridViewCellStyle1
+        Me.Column1.HeaderText = "i"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 37
+        '
+        'Column2
+        '
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        Me.Column2.DefaultCellStyle = DataGridViewCellStyle2
+        Me.Column2.HeaderText = "x"
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 90
+        '
+        'Column3
+        '
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter
+        Me.Column3.DefaultCellStyle = DataGridViewCellStyle3
+        Me.Column3.HeaderText = "y"
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 90
+        '
+        'Column4
+        '
+        DataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter
+        Me.Column4.DefaultCellStyle = DataGridViewCellStyle4
+        Me.Column4.HeaderText = "z"
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 90
+        '
+        'Column5
+        '
+        DataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter
+        Me.Column5.DefaultCellStyle = DataGridViewCellStyle5
+        Me.Column5.HeaderText = "error x"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 90
+        '
+        'Column6
+        '
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter
+        Me.Column6.DefaultCellStyle = DataGridViewCellStyle6
+        Me.Column6.HeaderText = "error y"
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 90
+        '
+        'Column7
+        '
+        DataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter
+        Me.Column7.DefaultCellStyle = DataGridViewCellStyle7
+        Me.Column7.HeaderText = "error z"
+        Me.Column7.Name = "Column7"
+        Me.Column7.Width = 90
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(525, 206)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Limpiar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Respuesta
+        '
+        Me.Respuesta.AutoSize = True
+        Me.Respuesta.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Respuesta.ForeColor = System.Drawing.Color.Maroon
+        Me.Respuesta.Location = New System.Drawing.Point(12, 546)
+        Me.Respuesta.Name = "Respuesta"
+        Me.Respuesta.Size = New System.Drawing.Size(0, 20)
+        Me.Respuesta.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(644, 601)
+        Me.Controls.Add(Me.Respuesta)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.salida)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.tcs)
+        Me.Controls.Add(Me.Panel1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Metodo de Jacobbi"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.salida, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents b3 As TextBox
+    Friend WithEvents b2 As TextBox
+    Friend WithEvents b1 As TextBox
+    Friend WithEvents a33 As TextBox
+    Friend WithEvents a23 As TextBox
+    Friend WithEvents a13 As TextBox
+    Friend WithEvents a32 As TextBox
+    Friend WithEvents a22 As TextBox
+    Friend WithEvents a12 As TextBox
+    Friend WithEvents a31 As TextBox
+    Friend WithEvents a21 As TextBox
+    Friend WithEvents a11 As TextBox
+    Friend WithEvents tcs As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents salida As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Respuesta As Label
+End Class
---
+++ Jacobbi/Form1.vb
@@ -0,0 +1,132 @@
+﻿Public Class Form1
+
+    Dim a1n(3), a2n(3), a3n(3), b(3), n, ec As Single
+    Dim f1, f2, f3 As Single
+    Dim x(50), y(50), z(50), erx(50), ery(50), erz(50) As Single
+    Dim i, redon As Integer
+    Dim cadena As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        a11.Text = ""
+        a12.Text = ""
+        a13.Text = ""
+        a21.Text = ""
+        a22.Text = ""
+        a23.Text = ""
+        a31.Text = ""
+        a32.Text = ""
+        a33.Text = ""
+        b1.Text = ""
+        b2.Text = ""
+        b3.Text = ""
+        tcs.Text = ""
+        Respuesta.Text = ""
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        salida.Rows.Clear()
+        n = tcs.Text
+        redon = n + 2
+        ec = 0.5 * 10 ^ (-n)
+        If Math.Abs(Convert.ToInt32(a13.Text) + Convert.ToInt32(a12.Text)) < Math.Abs(Convert.ToInt32(a11.Text)) Then
+            a1n(0) = a11.Text
+            a1n(1) = a12.Text
+            a1n(2) = a13.Text
+            b(0) = b1.Text
+            If Math.Abs(Convert.ToInt32(a21.Text) + Convert.ToInt32(a23.Text)) < Math.Abs(Convert.ToInt32(a22.Text)) Then
+                a2n(0) = a21.Text
+                a2n(1) = a22.Text
+                a2n(2) = a23.Text
+                b(1) = b2.Text
+                a3n(0) = a31.Text
+                a3n(1) = a32.Text
+                b(2) = b3.Text
+                a3n(2) = a33.Text
+            Else
+                a2n(0) = a31.Text
+                a2n(1) = a32.Text
+                a2n(2) = a33.Text
+                b(1) = b3.Text
+                a3n(0) = a21.Text
+                a3n(1) = a22.Text
+                a3n(2) = a23.Text
+                b(2) = b2.Text
+            End If
+        ElseIf Math.Abs(Convert.ToInt32(a22.Text) + Convert.ToInt32(a23.Text)) < Math.Abs(Convert.ToInt32(a21.Text)) Then
+            n = tcs.Text
+            a1n(0) = a21.Text
+            a1n(1) = a22.Text
+            a1n(2) = a23.Text
+            b(0) = b2.Text
+            redon = n + 2
+            ec = 0.5 * 10 ^ (-n)
+            If Math.Abs(Convert.ToInt32(a31.Text) + Convert.ToInt32(a33.Text)) < Math.Abs(Convert.ToInt32(a32.Text)) Then
+                a2n(0) = a31.Text
+                a2n(1) = a32.Text
+                a2n(2) = a33.Text
+                b(1) = b3.Text
+                a3n(0) = a11.Text
+                a3n(1) = a12.Text
+                a3n(2) = a13.Text
+                b(2) = b1.Text
+            Else
+                a2n(0) = a11.Text
+                a2n(1) = a12.Text
+                a2n(2) = a13.Text
+                b(1) = b1.Text
+                a3n(0) = a31.Text
+                a3n(1) = a32.Text
+                a3n(2) = a33.Text
+                b(2) = b3.Text
+            End If
+        Else
+            a1n(0) = a31.Text
+            a1n(1) = a32.Text
+            a1n(2) = a33.Text
+            b(0) = b3.Text
+            If Math.Abs(Convert.ToInt32(a11.Text) + Convert.ToInt32(a13.Text)) < Math.Abs(Convert.ToInt32(a12.Text)) Then
+                a2n(0) = a11.Text
+                a2n(1) = a12.Text
+                a2n(2) = a13.Text
+                b(1) = b1.Text
+                a3n(0) = a21.Text
+                a3n(1) = a22.Text
+                a3n(2) = a23.Text
+                b(2) = b2.Text
+            Else
+                a2n(0) = a21.Text
+                a2n(1) = a22.Text
+                a2n(2) = a23.Text
+                b(1) = b2.Text
+                a3n(0) = a11.Text
+                a3n(1) = a12.Text
+                a3n(2) = a13.Text
+                b(2) = b1.Text
+            End If
+        End If
+        i = 0
+        erx(0) = 1
+        ery(0) = 1
+        erz(0) = 1
+        x(0) = 0
+        y(0) = 0
+        z(0) = 0
+        salida.Rows.Add(i, Math.Round(x(i), redon), Math.Round(y(i), redon), Math.Round(z(i), redon), "-", "-", "-")
+        Do While (erx(i) > ec)
+            x(i + 1) = (b(0) - (a1n(1) * y(i)) - (a1n(2) * z(i))) / a1n(0)
+            y(i + 1) = (b(1) - (a2n(0) * x(i)) - (a2n(2) * z(i))) / a2n(1)
+            z(i + 1) = (b(2) - (a3n(0) * x(i)) - (a3n(1) * y(i))) / a3n(2)
+            erx(i + 1) = Math.Abs((x(i + 1) - x(i)) / x(i + 1))
+            ery(i + 1) = Math.Abs((y(i + 1) - y(i)) / y(i + 1))
+            erz(i + 1) = Math.Abs((z(i + 1) - z(i)) / z(i + 1))
+            salida.Rows.Add(i + 1, Math.Round(x(i + 1), redon), Math.Round(y(i + 1), redon), Math.Round(z(i + 1), redon), Math.Round(erx(i + 1), redon), Math.Round(ery(i + 1), redon), Math.Round(erz(i + 1), redon))
+            i = i + 1
+        Loop
+        Respuesta.Text = "Los valores aproximados de las raices son: X = " + Math.Round(x(i), redon).ToString() + " Y = " + Math.Round(y(i), redon).ToString() + " Z = " + Math.Round(z(i), redon).ToString()
+
+    End Sub
+End Class
